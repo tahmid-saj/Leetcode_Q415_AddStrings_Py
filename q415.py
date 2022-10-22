df17884340fa3a9be320fc2be2c7eff70c91aff7
Tahmid Sajin <64264899+tahmid-saj@users.noreply.github.com>
2022-10-21 20:13:37 -0400

    Create q415.py

---
+++ q415.py
@@ -0,0 +1,9 @@
+class Solution:
+    def addStrings(self, num1: str, num2: str) -> str:
+        self.x=num1
+        self.y=num2
+        a=int(self.x)
+        b=int(self.y)
+        s=a+b
+        
+        return str(s)
